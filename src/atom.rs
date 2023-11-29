f8664a3f570f764643d58b8441743fc03ecb4405
1risk <weiruofeng@ruofengx.cn>
2023-11-29 18:15:45 +0800

    atom::Atom更新 添加了几个初始化的函数

--- src/atom.rs
+++ src/atom.rs
@@ -1,14 +1,42 @@
-use crate::basic::{Value, EID};
+use crate::basic::{Value, EID, Marker};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, Hash, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialOrd, Ord, PartialEq, Eq)]
 pub struct Index {
     pub eid: EID,
-    pub prop: String,
+    pub prop: [u8; 8],
+}
+impl Index {
+    pub fn new() -> Self {
+        Index {
+            eid: EID(0),
+            prop: [0; 8],
+        }
+    }
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct Atom {
+    pub(crate) valid: bool,
     pub index: Index,
     pub value: Value,
+}
+impl Atom {
+    pub fn empty() -> Self {
+        Atom {
+            index: Index {
+                eid: EID(0),
+                prop: [0; 8],
+            },
+            value: Value::Mark(Marker::new("NOT_SET")),
+            valid: false,
+        }
+    }
+    pub fn new(index: Index, value: Value) -> Self {
+        Atom {
+            index,
+            value,
+            valid: true,
+        }
+    }
 }
