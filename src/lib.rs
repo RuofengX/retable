6d6c73eb648a4c530b181333b1d57dd0953191b2
ruofeng <i@ruofengx.com>
2023-12-12 13:10:44 +0800

    re-export Prop.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
-/// A two key value store.
-/// 
-/// Used by entropy-rs, The Game · Created by RuofengX.
-/// 
-/// 
-/// Features:
-/// * No-unsafe, also source code.
-/// * Thread-safe, build on top of sled and moka.
-/// * Persistent, check [`sled::Config`] to learn more about the database.
-/// 
+#![warn(missing_docs)]
+//! A two key value store.
+//! 
+//! Used by entropy-rs, The Game · Created by RuofengX.
+//! 
+//! 
+//! Features:
+//! * No-unsafe, also source code.
+//! * Thread-safe, build on top of sled and moka.
+//! * Persistent, check [`sled::Config`] to learn more about the database.
+//! 
 
 pub mod api;
 pub mod atom;
 pub mod basic;
-#[warn(missing_docs)]
 pub mod db;
 pub mod error;
 pub mod method;
 
-pub use db::Database;
+pub use db::{Database, Prop};
 pub use sled::Config;
