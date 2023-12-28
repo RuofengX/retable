efd574d76446045db9ef70804d728665e9dad543
ruofeng <i@ruofengx.cn>
2023-12-28 17:14:25 +0800

    Add basic struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,101 +1,97 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::{BTreeMap, BTreeSet, HashSet};
 
 use parking_lot::RwLock;
+use slab::Slab;
 
 mod binlog;
 
-struct Column<T: Default + Clone> {
-    slot: Vec<RwLock<T>>,
-    invalid: BTreeSet<usize>,
+pub struct Column<K: Copy + Ord, T: Default + Clone> {
+    valid: BTreeMap<K, usize>,
+    empty: BTreeSet<usize>,
+    slot: Vec<RwLock<Option<T>>>,
 }
-impl<T: Default + Clone> Column<T> {
-    fn new() -> Self {
-        Self::from(vec![])
-    }
 
-    fn from(raw: Vec<T>) -> Self {
-        let len = raw.len();
+impl<K: Copy + Ord, T: Default + Clone> Column<K, T> {
+    pub fn new() -> Self {
         Self {
-            slot: raw.into_iter().map(|x| RwLock::new(x)).collect(),
-            invalid: BTreeSet::new(),
+            valid: BTreeMap::new(),
+            empty: BTreeSet::new(),
+            slot: Vec::new(),
         }
     }
 
-    fn pre_allocate(&mut self, n: usize) {
-        let len = self.slot.len();
-        (len..(len + n)).for_each(|x| {
-            self.slot.push(RwLock::new(T::default()));
-            self.invalid.insert(x);
+    pub fn with_capacity(capacity: usize) -> Self {
+        let mut slot = Vec::new();
+        let mut empty = BTreeSet::new();
+        for ele in (0..capacity).into_iter() {
+            slot.push(RwLock::new(None));
+            empty.insert(ele);
+        }
+        Self {
+            valid: BTreeMap::new(),
+            empty,
+            slot,
+        }
+    }
+
+    pub fn pre_allocate(&mut self, size: usize) {
+        (0..size).into_iter().for_each(|_| {
+            let index = self.allocate_empty();
+            self.empty.insert(index);
         })
     }
 
-    fn extend_until(&mut self, k: usize) {
-        let len = self.slot.len();
-        let x = k + 1 - len;
-        if x > 0 {
-            self.pre_allocate(x)
-        }
-    }
-
-    fn contains_key(&self, k: usize) -> bool {
-        if k >= self.slot.len() {
-            return false;
-        }
-        if self.invalid.contains(&k) {
-            return false;
-        }
-        true
-    }
-
-    fn create(&mut self, v: T) -> usize {
-        if self.invalid.is_empty() {
-            self.pre_allocate(1);
-        }
-        let index = self.invalid.pop_first().unwrap();
-        let mut entry = unsafe { self.slot.get_unchecked(index).write() };
-        *entry = v;
-        index
-    }
-
-    fn read(&self, k: usize) -> Option<T> {
-        if self.invalid.contains(&k) {
+    pub fn get(&self, key: &K) -> Option<T> {
+        if let Some(index) = self.valid.get(key) {
+            let entry = unsafe { self.slot.get_unchecked(*index).read() };
+            Some(entry.as_ref().unwrap().clone())
+        } else {
             return None;
         }
-        if let Some(value) = self.slot.get(k) {
-            Some(value.read().clone())
+    }
+
+    pub fn set(&mut self, key: &K, value: T) -> Option<T> {
+        if let Some(index) = self.valid.get(key) {
+            self.update_exist(*index, value)
         } else {
+            let index = self.create_any(value);
+            self.valid.insert(*key, index);
             None
         }
     }
+    // TODO: Add gc, iter, merge
 
-    fn update_no_extend(&mut self, k: usize, v: T) -> Option<T> {
-        if k >= self.slot.len() {
-            return None;
+}
+
+/// Base function
+impl<K: Copy + Ord, T: Default + Clone> Column<K, T> {
+    fn create_any(&mut self, v: T) -> usize {
+        if let Some(index) = self.empty.pop_first() {
+            let mut entry = unsafe { self.slot.get_unchecked_mut(index).write() };
+            *entry = Some(v);
+            index
+        } else {
+            self.slot.push(RwLock::new(Some(v)));
+            self.slot.len()
         }
-        if self.invalid.contains(&k) {
-            return None;
-        }
-        let mut entry = unsafe { self.slot.get_unchecked_mut(k).upgradable_read() };
-        let old = entry.clone();
-        entry.with_upgraded(|x| *x = v);
-        Some(old)
     }
 
-    fn update(&mut self, k: usize, v: T) -> Option<T> {
-        self.extend_until(k);
-        self.update(k, v)
+    fn allocate_empty(&mut self) -> usize {
+        self.slot.push(RwLock::new(None));
+        self.slot.len()
     }
 
-    fn delete(&mut self, k: usize) -> Option<T> {
-        if k >= self.slot.len() {
-            return None;
-        }
-        if self.invalid.contains(&k) {
-            return None;
-        }
-        let entry = unsafe { self.slot.get_unchecked_mut(k).read() };
+    fn update_exist(&self, index: usize, v: T) -> Option<T> {
+        let mut entry = unsafe { self.slot.get_unchecked(index).upgradable_read() };
         let old = entry.clone();
-        self.invalid.insert(k);
-        Some(old)
+        entry.with_upgraded(|x| *x = Some(v));
+        old
+    }
+
+    fn delete_exist(&self, index: usize) -> Option<T> {
+        let mut entry = unsafe { self.slot.get_unchecked(index).upgradable_read() };
+        let old = entry.clone();
+        entry.with_upgraded(|x| *x = None);
+        old
     }
 }
