471fe78807cf25429e3c71c06cce2b6f451ee17c
ruofeng <i@ruofengx.cn>
2023-12-28 14:32:10 +0800

    A usize version

--- src/lib.rs
+++ src/lib.rs
@@ -1,180 +1,101 @@
-mod binlog;
+use std::collections::{BTreeMap, BTreeSet};
 
 use parking_lot::RwLock;
-use std::collections::BTreeMap;
-use zerocopy::{AsBytes, FromBytes, FromZeroes};
 
-pub trait Key: Copy + Ord + Send {}
-pub trait Value: AsBytes + FromBytes + FromZeroes + Clone + Send {}
-impl<T> Key for T where T: Copy + Ord + Send {}
-impl<T> Value for T where T: AsBytes + FromBytes + FromZeroes + Clone + Send {}
+mod binlog;
 
-pub enum Command<K: Key, V: Value> {
-    Create((K, V)),
-    Update((K, V)),
-    Read(K),
-    Take(K),
-    Delete(K),
-
-    Merge((K, V)),
-    CompareAndSwap((K, V, V)), // Key, Old, New
+struct Column<T: Default + Clone> {
+    slot: Vec<RwLock<T>>,
+    invalid: BTreeSet<usize>,
 }
-
-pub trait MergeFn<K: Key, V: Value>: Fn(&K, Option<V>, V) -> Option<V> {}
-impl<T, K: Key, V: Value> MergeFn<K, V> for T where T: Fn(&K, Option<V>, V) -> Option<V> {}
-
-pub struct RawProp<K: Key, V: Value, M: MergeFn<K, V>> {
-    index: BTreeMap<K, usize>,
-    data: Vec<Option<V>>,
-    empty_slot: Vec<usize>,
-    merge_op: M,
-}
-
-/// CRUD api
-impl<K: Key, V: Value, M: MergeFn<K, V>> RawProp<K, V, M> {
-    pub fn create(&mut self, k: &K, v: V) {
-        if self.contains_key(&k) {
-            unsafe { self.update_uncheck(k, v) };
-        } else {
-            unsafe { self.create_uncheck(k, v) }
-        }
+impl<T: Default + Clone> Column<T> {
+    fn new() -> Self {
+        Self::from(vec![])
     }
 
-    pub fn read(&self, k: &K) -> Option<V> {
-        if let Some(&index) = self.index.get(k) {
-            unsafe { self.read_uncheck(index).clone() }
-        } else {
-            None
-        }
-    }
-
-    pub fn update(&mut self, k: &K, v: V) -> Option<V> {
-        if !self.contains_key(&k) {
-            unsafe { self.create_uncheck(k, v) };
-            None
-        } else {
-            Some(unsafe { self.update_uncheck(k, v) })
-        }
-    }
-
-    pub fn delete(&mut self, k: &K) -> Option<V> {
-        if let Some(index) = self.index.remove(k) {
-            self.empty_slot.push(index);
-            unsafe { self.data.get_unchecked(index) }.clone()
-        } else {
-            None
-        }
-    }
-}
-
-impl<K: Key, V: Value, M: MergeFn<K, V>> RawProp<K, V, M> {
-    pub fn new(merge_op: M) -> RawProp<K, V, M> {
-        RawProp::<K, V, M> {
-            index: BTreeMap::new(),
-            data: Vec::new(),
-            empty_slot: Vec::new(),
-            merge_op,
-        }
-    }
-
-    /// Merge one value into self, return the old value.
-    pub fn merge(&mut self, key: &K, delta: V) -> Option<V> {
-        let old = self.read(key);
-        let new = (self.merge_op)(key, old, delta);
-        if let Some(new) = new{
-            self.update(key, new)
-        } else {
-            self.delete(key)
-        }
-    }
-}
-
-/// Interact with self
-impl<K: Key, V: Value, M: MergeFn<K, V>> RawProp<K, V, M> {
-    pub fn contains_key(&self, key: &K) -> bool {
-        self.index.contains_key(key)
-    }
-}
-
-/// Private method
-impl<K: Key, V: Value, M: MergeFn<K, V>> RawProp<K, V, M> {
-    /// # Panic
-    /// Panic when key not exists.
-    #[inline]
-    fn get_index_unchecked(&self, k: &K) -> usize {
-        self.index.get(k).unwrap().clone()
-    }
-
-    /// Safety: entry must valid.
-    #[inline]
-    unsafe fn get_value_unchecked(&self, index: usize) -> &Option<V> {
-        self.data.get_unchecked(index)
-    }
-
-    /// Safety: k must not exist before.
-    #[inline]
-    pub unsafe fn create_uncheck(&mut self, k: &K, v: V) {
-        let len = self.data.len();
-        self.data.push(Some(v));
-        self.index.insert(*k, len);
-    }
-
-    /// Safety: k must exist before.
-    #[inline]
-    pub unsafe fn read_uncheck(&self, index: usize) -> &Option<V> {
-        self.get_value_unchecked(index)
-    }
-
-    /// Safety: k must exist before.
-    #[inline]
-    pub unsafe fn update_uncheck(&mut self, k: &K, v: V) -> V {
-        let index = self.get_index_unchecked(k);
-        let entry = self.data.get_unchecked_mut(index);
-        let old = entry.as_ref().unwrap().clone();
-        *entry = Some(v);
-        old
-    }
-}
-
-pub struct Prop<K: Key, V: Value, M: MergeFn<K, V>> {
-    inner: RwLock<RawProp<K, V, M>>,
-}
-impl<K: Key, V: Value, M: MergeFn<K, V>> Prop<K, V, M> {
-    pub fn new(merge_op: M) -> Prop<K, V, M> {
+    fn from(raw: Vec<T>) -> Self {
+        let len = raw.len();
         Self {
-            inner: RwLock::new(RawProp::<K, V, M> {
-                index: BTreeMap::new(),
-                data: Vec::new(),
-                empty_slot: Vec::new(),
-                merge_op,
-            }),
+            slot: raw.into_iter().map(|x| RwLock::new(x)).collect(),
+            invalid: BTreeSet::new(),
         }
     }
-}
 
-impl<K: Key, V: Value, M: MergeFn<K, V>> Prop<K, V, M> {
-    pub fn create(&self, key: &K, value: V) {
-        let mut wtx = self.inner.write();
-        wtx.create(key, value)
+    fn pre_allocate(&mut self, n: usize) {
+        let len = self.slot.len();
+        (len..(len + n)).for_each(|x| {
+            self.slot.push(RwLock::new(T::default()));
+            self.invalid.insert(x);
+        })
     }
 
-    pub fn read(&self, key: &K) -> Option<V> {
-        let rtx = self.inner.read();
-        rtx.read(key)
+    fn extend_until(&mut self, k: usize) {
+        let len = self.slot.len();
+        let x = k + 1 - len;
+        if x > 0 {
+            self.pre_allocate(x)
+        }
     }
 
-    pub fn update(&self, key: &K, value: V) -> Option<V> {
-        let mut wtx = self.inner.write();
-        wtx.update(key, value)
+    fn contains_key(&self, k: usize) -> bool {
+        if k >= self.slot.len() {
+            return false;
+        }
+        if self.invalid.contains(&k) {
+            return false;
+        }
+        true
     }
 
-    pub fn delete(&self, key: &K) -> Option<V> {
-        let mut wtx = self.inner.write();
-        wtx.delete(key)
+    fn create(&mut self, v: T) -> usize {
+        if self.invalid.is_empty() {
+            self.pre_allocate(1);
+        }
+        let index = self.invalid.pop_first().unwrap();
+        let mut entry = unsafe { self.slot.get_unchecked(index).write() };
+        *entry = v;
+        index
     }
 
-    pub fn merge(&self, key:&K, delta: V) -> Option<V>{
-        let mut wtx = self.inner.write();
-        wtx.merge(key, delta)
+    fn read(&self, k: usize) -> Option<T> {
+        if self.invalid.contains(&k) {
+            return None;
+        }
+        if let Some(value) = self.slot.get(k) {
+            Some(value.read().clone())
+        } else {
+            None
+        }
+    }
+
+    fn update_no_extend(&mut self, k: usize, v: T) -> Option<T> {
+        if k >= self.slot.len() {
+            return None;
+        }
+        if self.invalid.contains(&k) {
+            return None;
+        }
+        let mut entry = unsafe { self.slot.get_unchecked_mut(k).upgradable_read() };
+        let old = entry.clone();
+        entry.with_upgraded(|x| *x = v);
+        Some(old)
+    }
+
+    fn update(&mut self, k: usize, v: T) -> Option<T> {
+        self.extend_until(k);
+        self.update(k, v)
+    }
+
+    fn delete(&mut self, k: usize) -> Option<T> {
+        if k >= self.slot.len() {
+            return None;
+        }
+        if self.invalid.contains(&k) {
+            return None;
+        }
+        let entry = unsafe { self.slot.get_unchecked_mut(k).read() };
+        let old = entry.clone();
+        self.invalid.insert(k);
+        Some(old)
     }
 }
