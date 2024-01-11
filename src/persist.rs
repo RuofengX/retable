24fe971da48e4eae9b8681528cf93c3c0afc802c
weiruofeng <weiruofeng@ruofengx.cn>
2024-01-11 23:14:12 +0800

    WIP iterator

--- src/persist.rs
+++ src/persist.rs
@@ -3,7 +3,7 @@ use zerocopy::{AsBytes, FromBytes};
 
 use lsm_tree::{Config, Tree};
 
-use crate::protocol::{LogWriter, MergeAssign};
+use crate::protocol::{LogWriter, MergeAssign, LogReader};
 
 pub struct LsmPersist<K, V, D> {
     inner: Tree,
@@ -47,4 +47,16 @@ where
     fn delete(&self, key: &K) {
         self.inner.remove(key.as_bytes()).unwrap();
     }
+}
+
+impl<K,V,D>Iterator for LsmPersist<K,V,D> {
+    type Item = (K,V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!();
+        let a = self.inner.iter();
+        let b = a.next();
+    }
+}
+impl <K,V,D> LogReader<K,V,> for LsmPersist<K,V,D>{
 }
